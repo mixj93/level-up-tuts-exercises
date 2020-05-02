04a90beb937423d64ddccfe5a12e7d920c3481ab
Li Xueli <mixj93@163.com>
2020-05-02 09:45:29 +0800

    Add Pro Gatsby 2

---
+++ pro-gatsby-2/README.md
@@ -0,0 +1,1 @@
+# Pro Gatsby 2
---
+++ pro-gatsby-2/gatsbySite/src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        reiciendis amet perferendis ad cum eum laborum odio, nihil minus illum
+        illo, rem, dicta dolor magni quibusdam recusandae! Voluptate,
+        necessitatibus est!
+      </p>
+    </Layout>
+  )
+}
+
+export default About
